a7df650163fd4daa7f7fe79d268f251bdd45976a
Rob <robgmerrill@gmail.com>
2022-08-09 20:50:07 -0700

    New nav

--- pages/contact.js
+++ pages/contact.js
@@ -1,11 +1,12 @@
 import Contact from "../components/Contact";
 import Footer from "../components/Footer";
 import Nav from "../components/Nav";
+import TempNav from "../components/TempNav";
 
 export default function ContactPage() {
   return (
     <>
-      <Nav />
+      <TempNav />
       <Contact />
       <Footer />
     </>
