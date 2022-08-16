f8585d8821da93e88759b49acf8fe124018912c5
Rob <robgmerrill@gmail.com>
2022-08-15 21:19:43 -0700

    More stuff

--- components/Footer.js
+++ components/Footer.js
@@ -4,7 +4,9 @@ export default function Footer() {
   const date = new Date();
   return (
     <div className="fixed bottom-0 flex justify-center items-center w-full h-16 b-4 bg-white space-x-4">
-      <div>&copy; {date.getYear() + 1900} Congee Club Kids</div>
+      <div className="text-black">
+        &copy; {date.getYear() + 1900} Congee Club Kids
+      </div>
 
       <RiFacebookCircleLine size={40} color="#4267B2"></RiFacebookCircleLine>
       <RiInstagramLine size={40} color="#8a3ab9"></RiInstagramLine>
