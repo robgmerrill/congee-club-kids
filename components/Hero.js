497b3633731a4c3b734ebb265ef4f53624181773
Rob <robgmerrill@gmail.com>
2022-08-16 20:50:25 -0700

    more

--- components/Hero.js
+++ components/Hero.js
@@ -6,36 +6,32 @@ export default function Hero() {
   return (
     <>
       <div
+        className="flex justify-center items-center flex-col md:flex-row"
         style={{ backgroundColor: "#97FFE5", height: "calc(100vh - 100px)" }}
       >
-        <div
-          className="flex justify-center items-center flex-col md:flex-row"
-          style={{}}
-        >
-          <div className="md:w-1/2">
-            <div className="flex justify-center items-center flex-col text-6xl md:text-9xl">
-              <h2>Congee</h2>
-              <p>Club Kids</p>
-            </div>
-            <div className="text-2xl md:text-4xl flex justify-center">
-              <motion.button
-                whileHover={{ scale: 1.1 }}
-                whileTap={{ scale: 0.9 }}
-                className="bg-yellow-300 rounded-3xl  p-4 w-64"
-              >
-                Button
-              </motion.button>
-            </div>
+        <div className="md:w-1/2">
+          <div className="flex justify-center items-center flex-col text-6xl md:text-9xl">
+            <h2>Congee</h2>
+            <p>Club Kids</p>
           </div>
+          <div className="text-2xl md:text-4xl flex justify-center">
+            <motion.button
+              whileHover={{ scale: 1.1 }}
+              whileTap={{ scale: 0.9 }}
+              className="bg-yellow-300 rounded-3xl  p-4 w-64"
+            >
+              Button
+            </motion.button>
+          </div>
+        </div>
 
-          <div className="md:w-1/2 flex justify-center items-center">
-            <Image
-              src={steamer}
-              height="350px"
-              width="350px"
-              alt="steamer basket"
-            />
-          </div>
+        <div className="md:w-1/2 flex justify-center items-center">
+          <Image
+            src={steamer}
+            height="350px"
+            width="350px"
+            alt="steamer basket"
+          />
         </div>
       </div>
     </>
