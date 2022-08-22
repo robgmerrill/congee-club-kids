eabf791ef66f8c453b0fc36403e864e444a4aff9
Rob <robgmerrill@gmail.com>
2022-08-22 15:36:48 -0700

    add fireworkds

--- components/Form.js
+++ components/Form.js
@@ -1,11 +1,64 @@
 import { useForm, ValidationError } from "@formspree/react";
 
-import { useRef, useEffect } from "react";
+import { useRef, useEffect, useCallback } from "react";
+import ReactCanvasConfetti from "react-canvas-confetti";
+
+const canvasStyles = {
+  position: "fixed",
+  pointerEvents: "none",
+  width: "100%",
+  height: "100%",
+  top: 0,
+  left: 0,
+};
 
 export default function ContactForm() {
   const [state, handleSubmit] = useForm("xknengrp");
 
   const emailRef = useRef();
+  const refAnimationInstance = useRef(null);
+
+  const getInstance = useCallback((instance) => {
+    refAnimationInstance.current = instance;
+  }, []);
+
+  const makeShot = useCallback((particleRatio, opts) => {
+    refAnimationInstance.current &&
+      refAnimationInstance.current({
+        ...opts,
+        origin: { y: 0.7 },
+        particleCount: Math.floor(200 * particleRatio),
+      });
+  }, []);
+
+  const fire = useCallback(() => {
+    makeShot(0.25, {
+      spread: 26,
+      startVelocity: 55,
+    });
+
+    makeShot(0.2, {
+      spread: 60,
+    });
+
+    makeShot(0.35, {
+      spread: 100,
+      decay: 0.91,
+      scalar: 0.8,
+    });
+
+    makeShot(0.1, {
+      spread: 120,
+      startVelocity: 25,
+      decay: 0.92,
+      scalar: 1.2,
+    });
+
+    makeShot(0.1, {
+      spread: 120,
+      startVelocity: 45,
+    });
+  }, [makeShot]);
 
   useEffect(() => {
     console.log(emailRef.current);
@@ -15,6 +68,10 @@ export default function ContactForm() {
   if (state.succeeded) {
     return <p>Thanks for your submission!</p>;
   }
+
+  // if (state.succeeded) {
+  //   fire();
+  // }
 
   return (
     <div
@@ -67,11 +124,13 @@ export default function ContactForm() {
 
         <button
           type="submit"
+          onClick={fire}
           disabled={state.submitting}
           className="bg-yellow-300 rounded-lg mt-4"
         >
           Submit
         </button>
+
         <ValidationError errors={state.errors} />
       </form>
     </div>
